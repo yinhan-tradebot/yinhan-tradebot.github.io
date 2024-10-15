215f8c03b9b4739c0041fda63d7e044c2a4be14f
tesla-cat <416640656@qq.com>
2024-10-16 02:09:39 +0800

    several fixes

---
+++ assets/TestPage-Cegyux9a.js
@@ -0,0 +1,1 @@
+import{at as e}from"./index-Bbc3EhUA.js";const t={};function r(c,n){return"hi"}const _=e(t,[["render",r]]);export{_ as default};
--- assets/TestPage-CkV0LkNc.js
+++
@@ -1,1 +0,0 @@
-import{as as e}from"./index-BkpUxzrX.js";const r={};function t(c,s){return"hi"}const _=e(r,[["render",t]]);export{_ as default};
