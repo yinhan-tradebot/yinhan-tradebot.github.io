d683c66313b9fbaaaf7eee7be17b0f0e77b0e008
tesla-cat <416640656@qq.com>
2024-10-16 20:40:37 +0800

    made tab names shorter

---
+++ assets/TestPage--UK9e4jU.js
@@ -0,0 +1,1 @@
+import{at as e}from"./index-C9rWOdv9.js";const t={};function r(c,n){return"hi"}const _=e(t,[["render",r]]);export{_ as default};
--- assets/TestPage-CUGO9Bj2.js
+++
@@ -1,1 +0,0 @@
-import{at as e}from"./index-BV7LSw0j.js";const t={};function r(c,n){return"hi"}const _=e(t,[["render",r]]);export{_ as default};
