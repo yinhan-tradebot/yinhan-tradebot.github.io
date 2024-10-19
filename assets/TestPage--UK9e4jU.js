5b4bbfbce3299a35ae7f91a09f64e067ff09ae9b
tesla-cat <416640656@qq.com>
2024-10-19 18:52:51 +0800

    trying to fix mobile scrolling

--- assets/TestPage--UK9e4jU.js
+++
@@ -1,1 +0,0 @@
-import{at as e}from"./index-C9rWOdv9.js";const t={};function r(c,n){return"hi"}const _=e(t,[["render",r]]);export{_ as default};
---
+++ assets/TestPage-DuTvklWw.js
@@ -0,0 +1,1 @@
+import{at as e}from"./index-BiG6ejtn.js";const t={};function r(c,n){return"hi"}const _=e(t,[["render",r]]);export{_ as default};
