629019b68547d802a60e8a70926315ae22995d2f
tesla-cat <416640656@qq.com>
2024-10-25 22:42:36 +0800

    added "view others' data"

--- assets/TestPage-B5AVII2f.js
+++
@@ -1,1 +0,0 @@
-import{at as e}from"./index-D5DI_C1i.js";const t={};function r(c,n){return"hi"}const _=e(t,[["render",r]]);export{_ as default};
---
+++ assets/TestPage-DeHdmt3p.js
@@ -0,0 +1,1 @@
+import{av as e}from"./index-BLmXzYaf.js";const r={};function t(c,n){return"hi"}const _=e(r,[["render",t]]);export{_ as default};
