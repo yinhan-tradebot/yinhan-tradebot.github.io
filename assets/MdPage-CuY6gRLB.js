e24fbc8ae5789aeb9fa16b36e978438b4ce618c0
tesla-cat <416640656@qq.com>
2024-11-19 17:33:21 +0800

    improved "View others' data"

--- assets/MdPage-CuY6gRLB.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./MarkDown-Dc6sGca-.js";import{o as s,j as o,$ as c}from"./index-CyfMGFyr.js";const a={class:"m10 scroll"},m={__name:"MdPage",setup(t){return(e,_)=>(s(),o("div",a,[c(r,{src:e.$route.query.src},null,8,["src"])]))}};export{m as default};
---
+++ assets/TestPage-T2y38ZG5.js
@@ -0,0 +1,1 @@
+import{at as e}from"./index-yu0Rf5Pw.js";const t={};function r(c,n){return"hi"}const _=e(t,[["render",r]]);export{_ as default};
