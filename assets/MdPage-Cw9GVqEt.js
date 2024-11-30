0a279f37a26c10677f9e97fcdc7682ba6f91ea22
tesla-cat <416640656@qq.com>
2024-11-30 10:52:30 +0800

    improve tables

---
+++ assets/MdPage-Cw9GVqEt.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./MarkDown-CrpMiOvo.js";import{b as s,e as c,i as o}from"./index-DUeoF5j7.js";const a={class:"m10 scroll"},m={__name:"MdPage",setup(t){return(e,_)=>(s(),c("div",a,[o(r,{src:e.$route.query.src},null,8,["src"])]))}};export{m as default};
