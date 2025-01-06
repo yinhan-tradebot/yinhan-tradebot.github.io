3c62c3d20573c87f73e7984b32f5daa2e361d4b0
tesla-cat <416640656@qq.com>
2025-01-06 23:31:30 +0800

    fixed create button

--- assets/MdPage-UuzDQTqb.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./MarkDown-BVUycLnT.js";import{b as s,e as c,i as o}from"./index-D4UXLyBh.js";const a={class:"m10 scroll"},m={__name:"MdPage",setup(t){return(e,_)=>(s(),c("div",a,[o(r,{src:e.$route.query.src},null,8,["src"])]))}};export{m as default};
